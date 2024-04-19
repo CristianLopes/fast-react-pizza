2c7e5abb9082cfafddc57df13c49c45c64f9468a
Cristian Lopes <cristianqibins@hotmail.com>
2024-04-19 14:28:11 -0300

    add loader indicator and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import Error from './ui/Error';
 import Home from './ui/Home';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import '../src/index.css';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />,
-  },
-  {
-    path: '/order/:orderId',
-    element: <Order />,
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <Link to='/'> Fast React Pizza Co </Link>
+
+      <p> UserName </p>
+    </div>
+  );
+}
+
+export default Header;
