6433e2055288dfc1e79200de4d410268d1c71a13
Cristian Lopes <cristianqibins@hotmail.com>
2024-04-22 19:16:29 -0300

    add tailwindcss
    Learning about text-styling, colors, responsive design

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -9,7 +9,9 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className='mb-4 text-sm text-stone-600 md:text-base'>
+        👋 Welcome! Please start by telling us your name:
+        </p>
 
       <input
         type="text"
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+    return (
+        <div className="hidden text-sm font-semibold">
+           Cristian 
+        </div>
+    )
+}
+
+export default Username
