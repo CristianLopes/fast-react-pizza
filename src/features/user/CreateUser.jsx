dac11688c09c30dc2ca38baf83dfb9acd7d38846
Cristian Lopes <cristianqibins@hotmail.com>
2024-04-22 19:30:30 -0300

    add save changes button

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -11,14 +11,17 @@ function CreateUser() {
     <form onSubmit={handleSubmit}>
       <p className='mb-4 text-sm text-stone-600 md:text-base'>
         👋 Welcome! Please start by telling us your name:
-        </p>
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className='w-72 px-2 py-2 border border-stone-200 rounded-lg'
       />
+
+      <button className='w-32 bg-yellow-500 px-2 py-2 rounded-full text-stone-50 font-semibold hover:bg-yellow-600 hover:text-stone-200'> Save Changes </button>
 
       {username !== '' && (
         <div>
