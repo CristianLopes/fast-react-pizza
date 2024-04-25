2282292abf6777f3930a0453ebde8cc81e461426
CristianLopes <cristianqibins@hotmail.com>
2024-04-24 22:18:52 -0300

    add position absolute to loader

--- src/ui/LoaderIndicator.jsx
+++ src/ui/LoaderIndicator.jsx
@@ -1,5 +1,10 @@
 function LoaderIndicator() {
-  return <div className='loader'></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center 
+    bg-slate-200/50 backdrop-blur-sm"> 
+      <div className='loader'></div>
+    </div> 
+    );
 }
 
 export default LoaderIndicator;
